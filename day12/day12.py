02f92773522798a1b47090525be6a36b7b13818b
Valliet Emmanuel <evalliet@fr.ibm.com>
2023-12-14 17:19:13 +0100

    Day 12

--- day12/day12.py
+++ day12/day12.py
@@ -1,39 +1,88 @@
 #!/usr/bin/env python3
-from functools import reduce
-from math import gcd
+import concurrent
+import concurrent.futures
 
 class Row:
-    def __init__(self, line):
+    def __init__(self, line, second=False):
         row, possibilities = line.strip().split(" ")
-        self.row = row
-        self.unknown = [i for i, v in enumerate(row) if v == '?']
-        self.matches = [int(i) for i in possibilities.split(",")]
+        if second:
+            self.row = "?".join([row for i in range(5)])
+            self.matches = [int(i) for i in possibilities.split(",")]
+            self.matches = self.matches + self.matches + self.matches + self.matches + self.matches
+        else:
+            self.row = row
+            self.matches = [int(i) for i in possibilities.split(",")]
+
         self.comb = 0
 
+
     def solve(self):
-        self.combination(self.row, self.matches)
+        print(self.matches)
+        self.combination(self.row)
         return self.comb
 
-    def combination(self, current_row, to_match):
-        index=current_row.find("?")
-        print(current_row)
+    def solve2(self):
+        print(f"{self.row} {self.matches}")
+        self.combination2(0, self.row, self.matches)
+        print(f"{self.row} {self.matches} ====> {self.comb}")
+        return self.comb
+
+    def combination(self, current_row):
+        index = current_row.find("?")
         if index < 0:
             if self.valid(current_row):
                 self.comb = self.comb + 1
-            print(f"out of ? {self.valid(current_row)} {self.comb}")
             return
         new_row_1 = current_row[:index] + "." + current_row[index + 1:]
-        if len(to_match) > 0 and index+to_match[0] < len(current_row):
-            ok = all([ c == "#" or c == "?" for c in current_row[index:index+to_match[0]]])
-            ok = ok and (index+to_match[0] == len(current_row)-1 or current_row[index+to_match[0]+1] == "?" or current_row[index+to_match[0]+1] == ".")
-            print(f"{index} {to_match[0]} {ok} {current_row[index:index+to_match[0]]} {index+to_match[0]} {current_row[index+to_match[0]] }")
-            if ok:
-                new_row_2 = current_row[:index] + "#"*to_match[0] + "." + current_row[index + 1 + to_match[0]:]
-                print(f"replace with #: {new_row_2}")
-                self.combination(new_row_2, to_match[1:])
+        new_row_2 = current_row[:index] + "#" + current_row[index + 1:]
+        self.combination(new_row_2)
+        self.combination(new_row_1)
 
-        self.combination(new_row_1, to_match)
+    def combination2(self, index, current_row, to_match):
+        #print(f"{index} {current_row}")
+        if index > len(current_row)-1:
+            if self.valid(current_row):
+                self.comb = self.comb + 1
+            return
 
+        match current_row[index]:
+            case '.':
+                self.combination2(index+1, current_row, to_match)
+            case '?':
+                new_row_1 = current_row[:index] + "." + current_row[index + 1:]
+                # if len(to_match) > 0:
+                #     new_row_2 = current_row[:index] + "#" + current_row[index + 1:]
+                #     self.combination2(index, new_row_2, to_match)
+
+                if len(to_match) > 0 and index+to_match[0] < len(current_row):
+                    ok = all([c == "#" or c == "?" for c in current_row[index:index+ to_match[0]]])
+                    ok = ok and (current_row[index + to_match[0]] == "?" or current_row[index + to_match[0]] == ".")
+                    if ok:
+                        new_row_2 = current_row[:index] + "#" * to_match[0] + "." + current_row[index + 1 + to_match[0]:]
+                        self.combination2(index + to_match[0], new_row_2, to_match[1:])
+                elif len(to_match) == 1 and index+to_match[0] == len(current_row):
+                    ok = all([c == "#" or c == "?" for c in current_row[index:index+ to_match[0]]]) and len(to_match) == 1
+                    if ok:
+                        new_row_2 = current_row[:index] + "#" * to_match[0] + "." + current_row[index + 1 + to_match[0]:]
+                        self.combination2(index + to_match[0], new_row_2, to_match[1:])
+
+                self.combination2(index, new_row_1, to_match)
+
+            case '#':
+                if len(to_match) > 0 and index+to_match[0] < len(current_row):
+                    ok = all([c == "#" or c == "?" for c in current_row[index:index+ to_match[0]]])
+                    ok = ok and (current_row[index + to_match[0]] == "?" or current_row[index + to_match[0]] == ".")
+                    #print(f"{ok} {index} {to_match[0]} {current_row[index:index + to_match[0]]} {index + to_match[0]} {current_row[index + to_match[0]] }")
+                    if ok:
+                        new_row_2 = current_row[:index] + "#" * to_match[0] + "." + current_row[index + 1 + to_match[0]:]
+                        #print(f"replace with #: {new_row_2}")
+                        self.combination2(index + to_match[0], new_row_2, to_match[1:])
+                elif len(to_match) == 1 and index+to_match[0] == len(current_row):
+                    ok = all([c == "#" or c == "?" for c in current_row[index:index+ to_match[0]]]) and len(to_match) == 1
+                    #print(f"ok = {ok}")
+                    if ok:
+                        new_row_2 = current_row[:index] + "#" * to_match[0] + "." + current_row[index + 1 + to_match[0]:]
+                        self.combination2(index + to_match[0], new_row_2, to_match[1:])
 
     def valid(self, a_row):
         return [len(s) for s in filter(None, a_row.split('.'))] == self.matches
@@ -46,13 +95,19 @@ class InputFile:
     def parse(self):
         rows = []
         for y, line in enumerate(self.lines):
-            rows.append(Row(line))
-        print(rows[0].solve())
+            rows.append(Row(line, second=True))
 
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [ executor.submit(r.solve2) for r in rows ]
+            returned_values = [ future.result() for future in concurrent.futures.as_completed(futures) ]
+            print(returned_values)
+            print(f"total overall: {sum(returned_values)}")
+        #print(sum([r.solve2() for r in rows]))
+        print("should be 525152")
 
 
 def main():
-    with open('sample.txt', 'r') as input_file:
+    with open('input.txt', 'r') as input_file:
         f = InputFile(input_file.readlines())
     f.parse()
 
