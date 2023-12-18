61d37cf08af8903903a982116930bd2d2f7c5cd9
Valliet Emmanuel <evalliet@fr.ibm.com>
2023-12-18 15:30:52 +0100

    Day 12

--- day16/day16.py
+++ day16/day16.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import time
 
 import numpy
@@ -6,44 +7,77 @@ import numpy
 class Beam:
     def __init__(self, tiles):
         self.tiles = tiles
-        self.direction = [1, 0]
-        self.position = [0, 0]
 
-    def walk(self):
-        if self.direction[0] + self.position[0] >= self.tiles.tiles.shape[1]:
+    def walk(self, x, y, dir_x, dir_y):
+        if x + dir_x >= self.tiles.tiles.shape[1]:
             return
-        if self.direction[1] + self.position[1] >= self.tiles.tiles.shape[0]:
+        if y + dir_y >= self.tiles.tiles.shape[0]:
             return
-        if self.direction[0] + self.position[0] < 0:
+        if x + dir_x < 0:
             return
-        if self.direction[1] + self.position[1] < 0:
+        if y + dir_y < 0:
             return
 
-        self.position[0] = self.position[0] + self.direction[0]
-        self.position[1] = self.position[1] + self.direction[1]
-        print(f"{self.direction} {self.position[0]},{self.position[1]}")
-        b = self.tiles.tiles[self.position[1]][self.position[0]].c
-        self.tiles.tiles[self.position[1]][self.position[0]].c = "o"
-        print(str(self.tiles))
-        self.tiles.tiles[self.position[1]][self.position[0]].c = b
+        # os.system('clear')
+        # print(f"{x},{y} {dir_x},{dir_y}")
+        # print(self.tiles.str(x, y, dir_x, dir_y))
+        #time.sleep(0.5)
+        x = x + dir_x
+        y = y + dir_y
+        self.tiles.tiles[y][x].visited = True
+        possibilities = self.tiles.tiles[y][x].visit([dir_x, dir_y])
+        while len(possibilities) == 1:
+            dir_x, dir_y = possibilities[0]
+            if x + dir_x >= self.tiles.tiles.shape[1]:
+                break
+            if y + dir_y >= self.tiles.tiles.shape[0]:
+                break
+            if x + dir_x < 0:
+                break
+            if y + dir_y < 0:
+                break
+            x = x + dir_x
+            y = y + dir_y
+            self.tiles.tiles[y][x].visited = True
+            possibilities = self.tiles.tiles[y][x].visit([dir_x, dir_y])
 
-        for n in self.tiles.tiles[self.position[1]][self.position[0]].visit(self.direction):
-            self.direction[0] = n[0]
-            self.direction[1] = n[1]
-            #print(str(self.tiles))
-            time.sleep(0.5)
-            self.walk()
+        if len(possibilities) > 1:
+            for p in possibilities:
+                self.walk(x, y, p[0], p[1])
 
 
 class Tiles:
     def __init__(self, tiles):
         self.tiles = numpy.array(tiles)
 
-    def __str__(self):
+    def sum(self):
+        s = 0
+        for y, line in enumerate(self.tiles):
+            for x, c in enumerate(line):
+                if c.visited:
+                    s = s +1
+        return s
+
+    def reset(self):
+        for y, line in enumerate(self.tiles):
+            for x, c in enumerate(line):
+                c.reset()
+
+    def str(self, a_x, a_y, dir_x, dir_y):
         s = ""
-        for line in self.tiles:
-            for x in line:
-                s = s + str(x)
+        for y, line in enumerate(self.tiles):
+            for x, c in enumerate(line):
+                if x == a_x and y == a_y:
+                    if dir_x == 1:
+                        s = s + ">"
+                    if dir_x == -1:
+                        s = s + "<"
+                    if dir_y == 1:
+                        s = s + "v"
+                    if dir_y == -1:
+                        s = s + "^"
+                else:
+                    s = s + str(c)
             s = s+"\n"
         return s
 
@@ -56,18 +90,21 @@ class Tile:
         self.y = y
         self.inputs = {}
 
+    def reset(self):
+        self.visited = False
+        self.inputs = {}
+
     def visit(self, direction):
         d = f"{direction[0]}{direction[1]}"
         if d in self.inputs:
             return []
         self.inputs[d] = 1
+        self.visited = True
         match self.c:
             case '.':
-                self.visited = True
                 return [(direction[0], direction[1])]
             case '\\':
-                if direction[0] != 0:
-                    return [(direction[1], direction[0])]
+                return [(direction[1], direction[0])]
             case '/':
                 return [(-direction[1], -direction[0])]
             case '|':
@@ -125,12 +162,31 @@ class InputFile:
 
         tiles = Tiles(a)
         beam = Beam(tiles)
-        beam.walk()
+        m = 0
+        for x in range(tiles.tiles.shape[1]):
+            print(f"x={x}  {m}")
+            beam.walk(x, -1, 0, 1)
+            m = max(m, tiles.sum())
+            tiles.reset()
+            beam.walk(x, tiles.tiles.shape[0], 0, -1)
+            m = max(m, tiles.sum())
+            tiles.reset()
+
+        for y in range(tiles.tiles.shape[0]):
+            print(f"y={y}  {m}")
+            beam.walk(-1, y, 1, 0)
+            m = max(m, tiles.sum())
+            tiles.reset()
+            beam.walk(tiles.tiles.shape[1], y, -1, 0)
+            m = max(m, tiles.sum())
+            tiles.reset()
+
         print(tiles)
+        print(tiles.sum())
 
 
 def main():
-    with open('sample.txt', 'r') as input_file:
+    with open('input.txt', 'r') as input_file:
         f = InputFile(input_file.readlines())
     f.parse()
 
